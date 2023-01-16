fa46fd4700a90a0e116623677c6a4317859978c3
QuentinBeauchet <quentinbeauchet@free.fr>
2023-01-16 17:26:46 +0100

    db

--- floor-server/index.js
+++ floor-server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 var cors = require("cors");
 const mariadb = require("mariadb");
+var bodyParser = require("body-parser");
 var app = express();
 
 const port = process.env.PORT;
@@ -10,6 +11,8 @@ const doors = {
   2: "http://localhost:3011",
   3: "http://localhost:3012",
 };
+
+app.use(bodyParser.json());
 
 app.use(cors());
 
@@ -37,16 +40,32 @@ app.get("/api/user", (req, res) => {
       .query(
         `SELECT DISTINCT users.*
       FROM users
-      JOIN \`user permissions\` ON users.id = \`user permissions\`.\`user id\`
+      JOIN \`user permissions\` ON users.rfid = \`user permissions\`.\`user rfid\`
       JOIN \`door permissions\`
       JOIN doors
       WHERE users.rfid = '${req.query.rfid}' AND \`door permissions\`.\`door id\` = '${req.query.door_id}' AND \`user permissions\`.\`permission id\` = \`door permissions\`.\`permission id\`;`
       )
       .then((rows) => {
-        if (!rows.length) {
-          res.sendStatus(404);
-        }
-        res.send(rows[0]);
+        rows.length ? res.send(rows[0]) : res.sendStatus(404);
+        conn.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        conn.end();
+      });
+  });
+});
+
+app.post("/api/user/add", (req, res) => {
+  buf = Buffer.from(req.body.picture).toString("base64");
+  pool.getConnection().then((conn) => {
+    conn
+      .query(
+        `INSERT INTO \`users\` ( \`rfid\`,\`first name\`, \`last name\`, \`picture\`) VALUES
+        ('${req.body.rfid}', '${req.body["first name"]}',  '${req.body["last name"]}',  '${buf}');`
+      )
+      .then(() => {
+        res.sendStatus(200);
         conn.end();
       })
       .catch((err) => {
